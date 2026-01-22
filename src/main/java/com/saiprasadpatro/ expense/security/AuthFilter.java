37ff3e3fdc63b5f10776d8b3fab39020049579c2
Saiprasadpatro <131334679+Saiprasadpatro@users.noreply.github.com>
2026-01-22 23:07:57 +0530

    Authentication filter added

--- src/main/java/com/saiprasadpatro/ expense/security/AuthFilter.java
+++ src/main/java/com/saiprasadpatro/ expense/security/AuthFilter.java
@@ -8,7 +8,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletException; 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
