0f326930bd2390520028bd76f092bad17b8c8abe
Saiprasadpatro <131334679+Saiprasadpatro@users.noreply.github.com>
2026-01-22 22:55:52 +0530

    Add ApprovalRepository interface for Approval model

---
+++ src/main/java/com/saiprasadpatro/ expense/repository/ApprovalRepository.java
@@ -0,0 +1,8 @@
+package com.saiprasadpatro.expense.repository;
+
+import com.saiprasadpatro.expense.model.Approval;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApprovalRepository extends JpaRepository<Approval, Long> { }
