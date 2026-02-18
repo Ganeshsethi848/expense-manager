1e817e3216dd80ed2aa30a74407e43cf78576121
Ganeshsethi848 <ganeshsethi848@gmail.com>
2026-02-18 23:13:48 +0530

    Create ExpenseManagerApplication.java with Spring Boot application class

--- src/main/java/com/expensemanager/ExpenseManagerApplication.java
+++ src/main/java/com/expensemanager/ExpenseManagerApplication.java
@@ -2,8 +2,10 @@ package com.expensemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.expensemanager"})
 public class ExpenseManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExpenseManagerApplication.class, args);
